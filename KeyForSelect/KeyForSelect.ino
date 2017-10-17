94e94126c61d01066c310d9a979f8423613c65be
001-PC\001 <panasonic707@gmail.com>
2017-10-17 22:14:37 +0900

    I2C+KeyFunction Test

--- KeyForSelect/KeyForSelect.ino
+++ KeyForSelect/KeyForSelect.ino
@@ -1,78 +1,84 @@
-//#include "Keyboard.h"
-//#include <Wire.h>
-//
-////Ad: https://qiita.com/MergeCells/items/17bdc1c1fb35949195b5
-//
-//
-//#define UpKey 2
-//#define UpLed 3
-//#define DownKey 4
-//#define DownLed 5
-//#define LeftKey 6
-//#define LeftLed 7
-//#define RightKey 8
-//#define RightLed 9
-//#define EnterKey 10
-//#define EnterLed 11
-//#define EscKey 12
-//#define CoinKey 13
-//
-//
-//void setup() {
-//  Keyboard.begin();
-//  Wire.begin();
-//  Serial.begin(9600);
-//  
-//  pinMode(UpKey, INPUT_PULLUP);
-//  pinMode(UpLed, OUTPUT);
-//  pinMode(DownKey, INPUT_PULLUP);
-//  pinMode(DownLed, OUTPUT);
-//  pinMode(LeftKey, INPUT_PULLUP);
-//  pinMode(LeftLed, OUTPUT);
-//  pinMode(RightKey, INPUT_PULLUP);
-//  pinMode(RightLed, OUTPUT);
-//  pinMode(EnterKey, INPUT_PULLUP);
-//  pinMode(EnterLed, OUTPUT);
-//  pinMode(EscKey, INPUT_PULLUP);
-//  pinMode(CoinKey, INPUT_PULLUP);
-//}
-//
-//
-//void loop() {
-//   Wire.requestFrom(8, 1);// request 1 bytes from Slave ID #8
-//    
-//  if(digitalRead(UpKey) == LOW) KeyFunction(UpKey, KEY_UP_ARROW);
-//  if(digitalRead(DownKey) == LOW) KeyFunction(DownKey, KEY_DOWN_ARROW);
-//  if(digitalRead(LeftKey) == LOW) KeyFunction(LeftKey, KEY_LEFT_ARROW);
-//  if(digitalRead(RightKey) == LOW) KeyFunction(RightKey, KEY_RIGHT_ARROW);
-//  if(digitalRead(EnterKey) == LOW) KeyFunction(EnterKey, KEY_RETURN);
-//  if(digitalRead(EscKey) == LOW) KeyFunction(EscKey, KEY_ESC);
-//  if(digitalRead(CoinKey) == LOW) KeyFunction(CoinKey, KEY_INSERT);
-//  delay(100);
-//}
-//
-//
-//void KeyFunction(char Key, char KeyType){
-//    Keyboard.press(KeyType);
-//    delay(100);
-//    Keyboard.releaseAll();
-//    while(digitalRead(Key) == LOW);
-//}
+#include "Keyboard.h"
 #include <Wire.h>
 
+//Ad: https://qiita.com/MergeCells/items/17bdc1c1fb35949195b5
+
+
+#define UpKey 2
+#define UpLed 3
+#define DownKey 4
+#define DownLed 5
+#define LeftKey 6
+#define LeftLed 7
+#define RightKey 8
+#define RightLed 9
+#define EnterKey 10
+#define EnterLed 11
+#define EscKey 12
+#define CoinKey 13
+
+
 void setup() {
+  Keyboard.begin();
   Wire.begin();
   Serial.begin(9600);
+  
+  pinMode(UpKey, INPUT_PULLUP);
+  pinMode(UpLed, OUTPUT);
+  pinMode(DownKey, INPUT_PULLUP);
+  pinMode(DownLed, OUTPUT);
+  pinMode(LeftKey, INPUT_PULLUP);
+  pinMode(LeftLed, OUTPUT);
+  pinMode(RightKey, INPUT_PULLUP);
+  pinMode(RightLed, OUTPUT);
+  pinMode(EnterKey, INPUT_PULLUP);
+  pinMode(EnterLed, OUTPUT);
+  pinMode(EscKey, INPUT_PULLUP);
+  pinMode(CoinKey, INPUT_PULLUP);
 }
 
+
+
+
 void loop() {
-  Wire.requestFrom(8, 1);// request 1 bytes from Slave ID #8
-
-  while (Wire.available()) {
+   Wire.requestFrom(8, 1);// request 1 bytes from Slave ID #8
+   while (Wire.available()) {
     char b = Wire.read();
-    if (b == '0') Serial.println("ZERO");
-    if (b == '1') Serial.println("ONE");
-  }
 
-  delay(500);
+//    KeyLedFunction
+    switch (b) {
+      case '0':
+        Serial.println("ZERO");
+        break;
+      case '1':
+        Serial.println("ONE");
+        break;
+      default:
+        Serial.println("OTHER");
+        break;
+    }
+    
+
+
+  }
+    //  KeyFunction
+    if(digitalRead(UpKey) == LOW) KeyFunction(UpKey, KEY_UP_ARROW);
+    if(digitalRead(DownKey) == LOW) KeyFunction(DownKey, KEY_DOWN_ARROW);
+    if(digitalRead(LeftKey) == LOW) KeyFunction(LeftKey, KEY_LEFT_ARROW);
+    if(digitalRead(RightKey) == LOW) KeyFunction(RightKey, KEY_RIGHT_ARROW);
+    if(digitalRead(EnterKey) == LOW) KeyFunction(EnterKey, KEY_RETURN);
+    if(digitalRead(EscKey) == LOW) KeyFunction(EscKey, KEY_ESC);
+    if(digitalRead(CoinKey) == LOW) KeyFunction(CoinKey, KEY_INSERT); 
+    delay(100);
+//  delay(500);
+
+
+}
+
+
+void KeyFunction(char Key, char KeyType){
+    Keyboard.press(KeyType);
+    delay(100);
+    Keyboard.releaseAll();
+    while(digitalRead(Key) == LOW);
 }
