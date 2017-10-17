46d6854d1cea96f3a121bf1a4bbdd5f89f3f7b8e
001-PC\001 <panasonic707@gmail.com>
2017-10-17 21:31:06 +0900

    I2C Test CharType

--- LightingMaster/LightingMaster.ino
+++ LightingMaster/LightingMaster.ino
@@ -1,23 +1,23 @@
 #include <Wire.h>
 
-byte b=0;
-char c;
+char b=0;
 
 void setup() {
   Wire.begin(8);// Slave ID #8
   Wire.onRequest(requestEvent);
+  
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+ if (Serial.available() > 0) {
+  b = Serial.read();
+ }
 }
 
 void requestEvent() {
-  
-     if (Serial.available() > 0) {  // データをIDEから受信したら処理開始
-          c = Serial.read() ;       // データを１バイト読込む
-          Serial.write(c) ;         // そのままデータを送り返す
-Wire.write(c);
-     }
 //  Wire.write(b++);
+//  b = Serial.read();
+  if (b != '\0') Wire.write(b);
+  Serial.println(b);
 }
